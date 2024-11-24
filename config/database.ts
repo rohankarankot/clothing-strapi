60695c6f8472017f61c6e2cace2fbf353bd5ef92
Rohan karankot <rohankarankot@gmail.com>
2024-11-24 16:57:38 +0530

    db corrected

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',  // Specify PostgreSQL as the client
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),  // Database host, default is 'localhost'
-      port: env('DATABASE_PORT', 5432),  // Database port, default is 5432 for PostgreSQL
+      host: env('DATABASE_HOST', 'localhost'),     // Database host, default is 'localhost'
+      port: env('DATABASE_PORT', 5432),            // Database port, default is 5432 for PostgreSQL
       database: env('DATABASE_NAME', 'clothing'),  // Database name (change to your desired database)
       user: env('DATABASE_USERNAME', 'postgres'),  // Your PostgreSQL username
       password: env('DATABASE_PASSWORD', 'rohan'), 
-      schema:env('DATABASE_SCHEMA','public')
+      schema:env('DATABASE_SCHEMA','public'),
+      ssl: {
+        rejectUnauthorized: false,                 // Allows self-signed certificates
+      },
     },
   },
 });
